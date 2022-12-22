232724f909deb88f0e1cc768a0f93341a1abb9ff
linnea <linnea.albertsson@gmail.com>
2022-12-22 11:13:02 +0100

    saves new user to db with same id as uid

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,179 +1,185 @@
-import * as WebBrowser from "expo-web-browser";
-import { useEffect, useState } from "react";
-import { StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect, useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
-import { Text, View } from "./Themed";
-import { Formik } from "formik";
-import * as yup from "yup";
-import { createUserWithEmailAndPassword } from "@firebase/auth";
-import { auth, db } from "../firebase";
-import { setDoc, doc, addDoc, collection, Timestamp } from "firebase/firestore";
+import { Text, View } from './Themed';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import {
+	createUserWithEmailAndPassword,
+	updatePassword,
+	updateProfile,
+} from '@firebase/auth';
+import { auth, db } from '../firebase';
+import { setDoc, doc, addDoc, collection, Timestamp } from 'firebase/firestore';
+import { setOneDoc } from '../helper';
 
 export default function Signup() {
-  const collectionRef = collection(db, "users");
+	const collectionRef = collection(db, 'users');
 
-  const [newUser, setNewUser] = useState({
-    email: "",
-    displayName: "",
-    password: "",
-    passwordConfirmation: "",
-  });
+	const [newUser, setNewUser] = useState({
+		email: '',
+		displayName: '',
+		password: '',
+		passwordConfirmation: '',
+	});
 
-  const addUser = async (email: string, displayName: string, id: string) => {
-    await setDoc(doc(db, "users", id), {
-      email: email,
-      displayName: displayName,
-    });
-  };
+	const addUserToDb = async () => {
+		const user = {
+			email: newUser.email,
+			displayName: newUser.displayName,
+			userID: auth.currentUser?.uid,
+			createdAt: Timestamp.now().toDate(),
+			photo: '',
+		};
+		setOneDoc('users', auth.currentUser?.uid, user);
+	};
+	const signup = async () => {
+		try {
+			const user = await createUserWithEmailAndPassword(
+				auth,
+				newUser.email,
+				newUser.password
+			).then(() => {
+				if (auth.currentUser) {
+					updateProfile(auth.currentUser, { displayName: newUser.displayName });
+				}
+			});
+			addUserToDb();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const signup = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        newUser.email,
-        newUser.password
-      );
-      await addDoc(collectionRef, {
-        email: newUser.email,
-        displayName: newUser.displayName,
-        userID: user.user.uid,
-        createdAt: Timestamp.now().toDate(),
-        photo: "",
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	return (
+		<View>
+			<Formik
+				initialValues={newUser}
+				onSubmit={(values) => {}}
+				validationSchema={yup.object().shape({
+					displayName: yup.string().required('Please, provide a displayName!'),
+					email: yup.string().email().required('Please, provide an email!'),
+					password: yup
+						.string()
+						.min(6, 'Password should be of minimum 6 characters length')
+						.required(),
+					passwordConfirmation: yup
+						.string()
+						.oneOf([yup.ref('password'), null], 'Passwords must match'),
+				})}
+			>
+				{({ values, errors, touched, handleBlur, handleChange }) => {
+					const { email, displayName, password, passwordConfirmation } = values;
 
-  return (
-    <View>
-      <Formik
-        initialValues={newUser}
-        onSubmit={(values) => {}}
-        validationSchema={yup.object().shape({
-          name: yup.string().required("Please, provide a displayName!"),
-          email: yup.string().email().required("Please, provide an email!"),
-          password: yup
-            .string()
-            .min(6, "Password should be of minimum 6 characters length")
-            .required(),
-          passwordConfirmation: yup
-            .string()
-            .oneOf([yup.ref("password"), null], "Passwords must match"),
-        })}
-      >
-        {({ values, errors, touched, handleBlur, handleChange }) => {
-          const { email, displayName, password, passwordConfirmation } = values;
+					useEffect(() => {
+						setNewUser({
+							email: email,
+							displayName: displayName,
+							password: password,
+							passwordConfirmation: passwordConfirmation,
+						});
+					}, [email, displayName, password]);
 
-          useEffect(() => {
-            setNewUser({
-              email: email,
-              displayName: displayName,
-              password: password,
-              passwordConfirmation: passwordConfirmation,
-            });
-          }, [email, displayName, password]);
+					return (
+						<View style={styles.container}>
+							<TextInput
+								style={styles.input}
+								placeholder="Username"
+								value={displayName}
+								onChangeText={handleChange('displayName')}
+								onBlur={handleBlur('displayName')}
+								autoCapitalize="none"
+							/>
+							{touched.displayName && errors.displayName && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.displayName}
+								</Text>
+							)}
+							<TextInput
+								style={styles.input}
+								placeholder="Email"
+								value={email}
+								onChangeText={handleChange('email')}
+								autoCapitalize="none"
+							/>
+							{touched.email && errors.email && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.email}
+								</Text>
+							)}
 
-          return (
-            <View style={styles.container}>
-              <TextInput
-                style={styles.input}
-                placeholder="Username"
-                value={displayName}
-                onChangeText={handleChange("displayName")}
-                onBlur={handleBlur("displayName")}
-                autoCapitalize="none"
-              />
-              {touched.displayName && errors.displayName && (
-                <Text style={{ fontSize: 10, color: "red" }}>
-                  {errors.displayName}
-                </Text>
-              )}
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={handleChange("email")}
-                autoCapitalize="none"
-              />
-              {touched.email && errors.email && (
-                <Text style={{ fontSize: 10, color: "red" }}>
-                  {errors.email}
-                </Text>
-              )}
+							<TextInput
+								style={styles.input}
+								placeholder="Password"
+								secureTextEntry
+								value={password}
+								onChangeText={handleChange('password')}
+								autoCapitalize="none"
+								onBlur={handleBlur('password')}
+							/>
+							{touched.password && errors.password && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.password}
+								</Text>
+							)}
 
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-                value={password}
-                onChangeText={handleChange("password")}
-                autoCapitalize="none"
-                onBlur={handleBlur("password")}
-              />
-              {touched.password && errors.password && (
-                <Text style={{ fontSize: 10, color: "red" }}>
-                  {errors.password}
-                </Text>
-              )}
+							<TextInput
+								style={styles.input}
+								placeholder="Password"
+								secureTextEntry
+								value={passwordConfirmation}
+								onChangeText={handleChange('passwordConfirmation')}
+								autoCapitalize="none"
+								onBlur={handleBlur('passwordConfirmation')}
+							/>
+							{touched.passwordConfirmation && errors.passwordConfirmation && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.passwordConfirmation}
+								</Text>
+							)}
 
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-                value={passwordConfirmation}
-                onChangeText={handleChange("passwordConfirmation")}
-                autoCapitalize="none"
-                onBlur={handleBlur("passwordConfirmation")}
-              />
-              {touched.passwordConfirmation && errors.passwordConfirmation && (
-                <Text style={{ fontSize: 10, color: "red" }}>
-                  {errors.passwordConfirmation}
-                </Text>
-              )}
-
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => signup()}
-                disabled={!values.email || !values.password}
-              >
-                <Text style={styles.buttonText}>Registrera dig</Text>
-              </TouchableOpacity>
-            </View>
-          );
-        }}
-      </Formik>
-    </View>
-  );
+							<TouchableOpacity
+								style={styles.button}
+								onPress={() => signup()}
+								disabled={!values.email || !values.password}
+							>
+								<Text style={styles.buttonText}>Registrera dig</Text>
+							</TouchableOpacity>
+						</View>
+					);
+				}}
+			</Formik>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    marginHorizontal: 50,
-  },
-  input: {
-    fontSize: 17,
-    height: 50,
-    width: 300,
-    color: "#fff",
-    backgroundColor: "#413C48",
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 6,
-  },
-  button: {
-    backgroundColor: "#FFFD54",
-    padding: 10,
-    borderRadius: 6,
-    width: 300,
-    height: 50,
-    marginTop: 10,
-  },
-  buttonText: {
-    color: "#201A28",
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: 17,
-  },
+	container: {
+		alignItems: 'center',
+		marginHorizontal: 50,
+	},
+	input: {
+		fontSize: 17,
+		height: 50,
+		width: 300,
+		color: '#fff',
+		backgroundColor: '#413C48',
+		marginBottom: 10,
+		padding: 10,
+		borderRadius: 6,
+	},
+	button: {
+		backgroundColor: '#FFFD54',
+		padding: 10,
+		borderRadius: 6,
+		width: 300,
+		height: 50,
+		marginTop: 10,
+	},
+	buttonText: {
+		color: '#201A28',
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: 17,
+	},
 });
--- helper.js
+++ helper.js
@@ -5,7 +5,7 @@ import {
 	getDoc,
 	setDoc,
 } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db } from './firebase';
 
 //Gets one document in a collection by doc id
 export const getOneDocById = async (collectionName, id) => {
