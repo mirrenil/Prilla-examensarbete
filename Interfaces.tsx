dbd389cf45580bd8ee6c77776178badefe637072
linnea <linnea.albertsson@gmail.com>
2022-12-23 09:43:25 +0100

    added interface for product

--- Interfaces.tsx
+++ Interfaces.tsx
@@ -11,6 +11,24 @@ export interface Review {
     id?: string
 }
 
-interface Tag {
+export interface Tag {
     name: string
+}
+
+export interface Product {
+    brand: string,
+    description: string,
+    flavor: string[],
+    format: string,
+    manufacturer: string,
+    name: string,
+    nicotine: number,
+    photo: string,
+    pouches: number,
+    // productID: string,
+    reviews: string[],
+    strength: number,
+    tags: Tag[],
+    type: string,
+    weigth: number
 }
