023f7679d8373b2d87dd2610dbdc396bb82c514d
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-28 14:52:37 +0100

    adds action types

--- redux/actions/index.ts
+++ redux/actions/index.ts
@@ -1,9 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Dispatch } from "redux";
+import firebase from "firebase/app";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "@reduxjs/toolkit";
+import { auth } from "../../firebase";
 
 // Action types
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+export const SET_USER = "SET_USER";
+export const GET_CURRENT_USER = "GET_CURRENT_USER";
 
 // Action creators
 export function login() {
@@ -12,6 +18,10 @@ export function login() {
 
 export function logout() {
   return { type: LOGOUT };
+}
+
+export function setUser(user: typeof auth | null) {
+  return { type: SET_USER, user };
 }
 
 // Thunks (async action creators)
@@ -42,5 +52,13 @@ export function loadLoginState() {
     } catch (error) {
       console.error(error);
     }
+  };
+}
+
+export function getCurrentUser() {
+  return (dispatch: ThunkDispatch<any, any, AnyAction>) => {
+    auth.onAuthStateChanged((user) => {
+      dispatch(setUser(auth || null));
+    });
   };
 }
