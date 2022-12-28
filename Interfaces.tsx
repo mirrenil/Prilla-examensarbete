db530070cc6bb2d1b64ba9a7e94a7836d81ef1c7
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-28 14:52:04 +0100

    adds props to user

--- Interfaces.tsx
+++ Interfaces.tsx
@@ -1,41 +1,42 @@
-
-
 export interface Review {
-    createdAt: Date,
-    description: string,
-    photo: string,
-    productID: string,
-    rating: number,
-    tags: Tag[],
-    userID: string,
-    id?: string
+  createdAt: Date;
+  description: string;
+  photo: string;
+  productID: string;
+  rating: number;
+  tags: Tag[];
+  userID: string;
+  id?: string;
 }
 
 export interface Tag {
-    name: string
+  name: string;
 }
 
 export interface Product {
-    brand: string,
-    description: string,
-    flavor: string[],
-    format: string,
-    manufacturer: string,
-    name: string,
-    nicotine: number,
-    photo: string,
-    pouches: number,
-    // productID: string,
-    reviews: string[],
-    strength: number,
-    tags: Tag[],
-    type: string,
-    weigth: number
+  brand: string;
+  description: string;
+  flavor: string[];
+  format: string;
+  manufacturer: string;
+  name: string;
+  nicotine: number;
+  photo: string;
+  pouches: number;
+  reviews: string[];
+  strength: number;
+  tags: Tag[];
+  type: string;
+  weigth: number;
 }
 
 export interface User {
-    createdAt: Date,
-    displayName: string,
-    email: string,
-    photo: string
+  id: string;
+  createdAt: Date;
+  displayName: string;
+  email: string;
+  photo: string;
+  liked: string[];
+  reviews: string[];
+  grade: number;
 }
