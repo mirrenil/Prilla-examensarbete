5d1a9e578e674b9bea7a1c4d7e2f4b295a3b8ebc
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-28 09:17:18 +0100

    configures store

--- redux/store/index.ts
+++ redux/store/index.ts
@@ -1,9 +1,33 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import rootReducers from "../reducers";
 import { getFirebase } from "react-redux-firebase";
+import { createFirebaseInstance } from "react-redux-firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+export const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+
+export const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users",
+    useFirestoreForProfile: true,
+  },
+};
 
 export default configureStore({
   reducer: rootReducers,
-  middleware: [...getDefaultMiddleware(), thunk],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: {
+          getFirebase,
+          createFirebaseInstance,
+        },
+      },
+    }).concat(thunk),
 });
