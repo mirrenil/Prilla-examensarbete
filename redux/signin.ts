54d0e1d6fd896eeea22c8b2ff72ce51149006b49
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-30 12:11:01 +0100

    removes out commented code

--- redux/signin.ts
+++ redux/signin.ts
@@ -1,38 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// same as [displayName, setdisplayName] = useState(null)
 const initialState = {
-  // reduxEmail: null,
-    displayName: "",
-    id: "",
-    email: ""
-  
+  displayName: "",
+  id: "",
+  email: "",
 };
 
 const userSignInSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    // same as setreduxEmail(action.payload)
     setActiveUser: (state, action) => {
-      // console.log('setActiveUser state : ', state )
-      console.log('setActiveUser action : ',  action.payload.currentUser)
-      // state.reduxEmail = action.payload.reduxEmail;
-      state.displayName = action.payload.currentUser.displayName
-      state.id = action.payload.currentUser.uid
-      state.email = action.payload.currentUser.email
-      console.log('state: ', state)
+      console.log("setActiveUser action : ", action.payload.currentUser);
+      state.displayName = action.payload.currentUser.displayName;
+      state.id = action.payload.currentUser.uid;
+      state.email = action.payload.currentUser.email;
+      console.log("state: ", state);
     },
     setSignOutState: (state) => {
-      // state.reduxEmail = null;
-      state = {displayName: "", id: "", email: ""}
+      state = { displayName: "", id: "", email: "" };
     },
   },
 });
 
 export const { setActiveUser, setSignOutState } = userSignInSlice.actions;
 
-// export const selectReduxEmail = (state: any) => state.user.reduxEmail;
 export const currentReduxUser = (state: any) => state.user;
 
 export default userSignInSlice.reducer;
