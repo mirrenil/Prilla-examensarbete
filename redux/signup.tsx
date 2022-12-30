18dcdfabdf2ed4aa640b8c65977821fef6c97717
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-30 12:10:48 +0100

    removes reducer for signup

--- redux/signup.tsx
+++ redux/signup.tsx
@@ -1,26 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-// same as [displayName, setdisplayName] = useState(null)
-const initialState = {
-  reduxDisplayName: null,
-  reduxEmail: null,
-};
+// // same as [displayName, setdisplayName] = useState(null)
+// const initialState = {
+//   reduxDisplayName: null,
+//   reduxEmail: null,
+// };
 
-const userSignUp = createSlice({
-  name: "newreduxuser",
-  initialState,
-  reducers: {
-    // same as setEmail(action.payload)
-    setNewUser: (state, action) => {
-      console.log('in userSignUp')
-      state.reduxDisplayName = action.payload.reduxDisplayName;
-      state.reduxEmail = action.payload.reduxEmail;
-    },
-  },
-});
-export const { setNewUser } = userSignUp.actions;
+// const userSignUp = createSlice({
+//   name: "newreduxuser",
+//   initialState,
+//   reducers: {
+//     // same as setEmail(action.payload)
+//     setNewUser: (state, action) => {
+//       console.log('in userSignUp')
+//       state.reduxDisplayName = action.payload.reduxDisplayName;
+//       state.reduxEmail = action.payload.reduxEmail;
+//     },
+//   },
+// });
+// export const { setNewUser } = userSignUp.actions;
 
-export const reduxDisplayName = (state: any) => state.user.displayName;
-export const reduxEmail = (state: any) => state.user.email;
+// export const reduxDisplayName = (state: any) => state.user.displayName;
+// export const reduxEmail = (state: any) => state.user.email;
 
-export default userSignUp.reducer;
+// export default userSignUp.reducer;
--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./signin";
-import newReduxUserReducer from "./signup";
 
 export default configureStore({
   reducer: {
     user: userReducer,
-    newreduxuser: newReduxUserReducer,
   },
 });
