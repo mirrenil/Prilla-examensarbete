b04f631fb37295462471865ade7356957102aee5
linnea <linnea.albertsson@gmail.com>
2022-12-30 14:27:54 +0100

    fixed so activity card gets img from reivew

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,13 +1,16 @@
 import * as ImagePicker from 'expo-image-picker';
 import { Constants } from 'expo-constants';
 import { useEffect, useState } from 'react';
-import { Button, Platform, Image } from 'react-native';
-import { result } from 'lodash';
+import { Platform, Image } from 'react-native';
 import { View, Text } from './Themed';
 import { MaterialIcons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const ImageUpload = () => {
+interface Props {
+	handleUpload: (a: any) => void;
+}
+
+const ImageUpload = ({ handleUpload }: Props) => {
 	const [image, setImage] = useState<any>(null);
 
 	useEffect(() => {
@@ -15,25 +18,33 @@ const ImageUpload = () => {
 	}, []);
 
 	const test = async () => {
-		if (Platform.OS !== 'web') {
-			const { status } =
-				await ImagePicker.requestMediaLibraryPermissionsAsync();
-			if (status !== 'granted') {
-				alert('Permission denied');
+		try {
+			if (Platform.OS !== 'web') {
+				const { status } =
+					await ImagePicker.requestMediaLibraryPermissionsAsync();
+				if (status !== 'granted') {
+					alert('Permission denied');
+				}
 			}
+		} catch (err) {
+			console.log(err);
 		}
 	};
 
 	const PickImage = async () => {
-		let result = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.All,
-			allowsEditing: true,
-			aspect: [4, 3],
-			quality: 1,
-		});
-		console.log(result);
-		if (!result.canceled) {
-			setImage(result.assets[0].uri);
+		try {
+			let result = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.All,
+				allowsEditing: true,
+				aspect: [4, 3],
+				quality: 1,
+			});
+			if (!result.canceled) {
+				setImage(result.assets[0].uri);
+				handleUpload(result.assets[0].uri);
+			}
+		} catch (err) {
+			console.log(err);
 		}
 	};
 
