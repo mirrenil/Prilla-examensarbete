ce80f62481025ec4eb645b3cd85319bd64bb5059
linnea <linnea.albertsson@gmail.com>
2023-01-02 16:45:57 +0100

    formatting only

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -1,96 +1,96 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { getOneDocById } from '../helper';
-import { Tag, Review, Product } from '../Interfaces';
-import { RatingDots } from './Rating';
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { getOneDocById } from "../helper";
+import { Tag, Review, Product } from "../Interfaces";
+import { RatingDots } from "./Rating";
 
 interface Props {
-	review: Review;
+  review: Review;
 }
 
 export const ReviewCard = ({ review }: Props) => {
-	const [product, setProduct] = useState<Product>();
-	const navigation = useNavigation();
+  const [product, setProduct] = useState<Product>();
+  const navigation = useNavigation();
 
-	useEffect(() => {
-		getProduct();
-	}, []);
+  useEffect(() => {
+    getProduct();
+  }, []);
 
-	const getProduct = async () => {
-		let data = await getOneDocById('produkter', review.productID);
-		if (data) {
-			setProduct(data as Product);
-		}
-	};
+  const getProduct = async () => {
+    let data = await getOneDocById("produkter", review.productID);
+    if (data) {
+      setProduct(data as Product);
+    }
+  };
 
-	if (product) {
-		return (
-			<View style={styles.wrapper}>
-				<View style={styles.productData}>
-					<Image style={styles.image} source={{ uri: product.Photo }} />
-					<View style={styles.textAndRating}>
-						<TouchableOpacity
-							onPress={() =>
-								navigation.navigate('Product', { id: review.productID })
-							}
-						>
-							<View style={styles.productText}>
-								<Text style={styles.textBold}>
-									{product.Brand + ' ' + product.Name}
-								</Text>
-								<Text>{product.Type}</Text>
-							</View>
-						</TouchableOpacity>
-						<View style={{flexDirection: 'row'}}>
-							<RatingDots rating={review.rating} />
-							<Text style={{marginLeft: 10}}>{review.rating}</Text>
-						</View>
-					</View>
-				</View>
-				<View style={styles.description}>
-					<Text>{review.description}</Text>
-				</View>
-			</View>
-		);
-	} else {
-		return (
-			<View>
-				<Text>Loading</Text>
-			</View>
-		);
-	}
+  if (product) {
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.productData}>
+          <Image style={styles.image} source={{ uri: product.photo }} />
+          <View style={styles.textAndRating}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("Product", { id: review.productID })
+              }
+            >
+              <View style={styles.productText}>
+                <Text style={styles.textBold}>
+                  {product.brand + " " + product.name}
+                </Text>
+                <Text>{product.type}</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <RatingDots rating={review.rating} />
+              <Text style={{ marginLeft: 10 }}>{review.rating}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.description}>
+          <Text>{review.description}</Text>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-	wrapper: {
-		marginTop: 10,
-		borderRadius: 6,
-		width: '90%',
-		padding: 10,
-		backgroundColor: 'rgba(255,255,255,0.5)',
-	},
-	image: {
-		height: 60,
-		width: 60,
-		flex: 1,
-	},
-	productData: {
-		flexDirection: 'row',
-	},
-	textAndRating: {
-		marginLeft: 10,
-		flex: 4,
-	},
-	productText: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	textBold: {
-		fontWeight: 'bold',
-	},
-	description: {
-		padding: 10,
-	},
+  wrapper: {
+    marginTop: 10,
+    borderRadius: 6,
+    width: "90%",
+    padding: 10,
+    backgroundColor: "rgba(255,255,255,0.5)",
+  },
+  image: {
+    height: 60,
+    width: 60,
+    flex: 1,
+  },
+  productData: {
+    flexDirection: "row",
+  },
+  textAndRating: {
+    marginLeft: 10,
+    flex: 4,
+  },
+  productText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textBold: {
+    fontWeight: "bold",
+  },
+  description: {
+    padding: 10,
+  },
 });
