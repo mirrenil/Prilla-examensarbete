231a4be569ef6dd78590076935c833c19bc64a9c
linnea <linnea.albertsson@gmail.com>
2023-01-02 11:36:36 +0100

    added scrollView, ActivityIndicator and fetching users reviews

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,6 +1,12 @@
 import { AntDesign } from '@expo/vector-icons';
 import { useEffect, useState } from 'react';
-import { StyleSheet, TouchableOpacity, Image, Button } from 'react-native';
+import {
+	StyleSheet,
+	TouchableOpacity,
+	Image,
+	Button,
+	ActivityIndicator,
+} from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { ActivityCard } from '../components/ActivityCard';
@@ -9,8 +15,9 @@ import { FavoritesCard } from '../components/FavoritesCard';
 import { RootTabScreenProps } from '../types';
 import { useSelector } from 'react-redux';
 import { currentReduxUser } from '../redux/signin';
-import { getAllDocsInCollection, getDocsWithSpecificValue } from '../helper';
+import { getDocsWithSpecificValue } from '../helper';
 import { Review } from '../Interfaces';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function ProfileScreen({
 	navigation,
@@ -21,14 +28,22 @@ export default function ProfileScreen({
 	const [reviews, setReviews] = useState<Review[]>([]);
 
 	useEffect(() => {
+		console.log('params id: ', route.params.id);
 		getReviews();
 	}, []);
 
 	const getReviews = async () => {
-
-    //använd för att hämta alla reviews som har userID == route.params.id
-    let data = await getDocsWithSpecificValue()
-
+		try {
+			let data = await getDocsWithSpecificValue(
+				'recensioner',
+				'userID',
+				route.params.id
+			);
+			console.log(data);
+			setReviews(data as Review[]);
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	const toggleButton = () => {
@@ -37,61 +52,65 @@ export default function ProfileScreen({
 
 	useEffect(() => {}, [user]);
 
-	return (
-		<View style={styles.screen}>
-			<View style={styles.container}>
-				<UserInfoCard />
-				{!user && (
-					<TouchableOpacity
-						style={[follow ? styles.borderButton : styles.button]}
-						onPress={toggleButton}
-					>
-						<Text
-							darkColor="#201A28"
-							lightColor="#201A28"
-							style={[follow ? styles.borderButtonText : styles.buttonText]}
+	if (user) {
+		return (
+			<ScrollView style={styles.screen}>
+				<View style={styles.container}>
+					<UserInfoCard />
+					{!user && (
+						<TouchableOpacity
+							style={[follow ? styles.borderButton : styles.button]}
+							onPress={toggleButton}
 						>
-							{follow ? 'Följer' : 'Följ'}{' '}
-							{follow ? (
-								<AntDesign name="down" size={14} color="white" />
-							) : null}
-						</Text>
-					</TouchableOpacity>
-				)}
-			</View>
+							<Text
+								darkColor="#201A28"
+								lightColor="#201A28"
+								style={[follow ? styles.borderButtonText : styles.buttonText]}
+							>
+								{follow ? 'Följer' : 'Följ'}{' '}
+								{follow ? (
+									<AntDesign name="down" size={14} color="white" />
+								) : null}
+							</Text>
+						</TouchableOpacity>
+					)}
+				</View>
 
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<View style={styles.favorites}>
-				<View style={styles.box}>
-					<Text lightColor="#fff" darkColor="#fff" style={styles.text}>
-						Favoriter <AntDesign name="right" size={16} color="white" />
-					</Text>
+				<View
+					style={styles.separator}
+					lightColor="#eee"
+					darkColor="rgba(255,255,255,0.1)"
+				/>
+				<View style={styles.favorites}>
+					<View style={styles.box}>
+						<Text lightColor="#fff" darkColor="#fff" style={styles.text}>
+							Favoriter <AntDesign name="right" size={16} color="white" />
+						</Text>
+					</View>
+					<FavoritesCard />
 				</View>
-				<FavoritesCard />
-			</View>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<View style={styles.activities}>
-				<View style={styles.box}>
-					<Text lightColor="#fff" darkColor="#fff" style={styles.text}>
-						Aktiviteter
-					</Text>
-					<AntDesign name="right" size={16} color="white" />
+				<View
+					style={styles.separator}
+					lightColor="#eee"
+					darkColor="rgba(255,255,255,0.1)"
+				/>
+				<View style={styles.activities}>
+					<View style={styles.box}>
+						<Text lightColor="#fff" darkColor="#fff" style={styles.text}>
+							Aktiviteter
+						</Text>
+						<AntDesign name="right" size={16} color="white" />
+					</View>
+					{/* should only display reviews by specific user */}
+					{reviews.map((review: any) => {
+						return <ActivityCard key={review.id} review={review} />;
+					})}
 				</View>
-				{/* should only display reviews by specific user */}
-				{reviews.map((review: any) => {
-					return <ActivityCard key={review.id} review={review} />;
-				})}
-			</View>
-		</View>
-	);
+			</ScrollView>
+		);
+	} else {
+		return <ActivityIndicator size="small" color="#0000ff" />;
+	}
 }
 
 const styles = StyleSheet.create({
