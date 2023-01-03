c82fc613920d82665f203da81bc9fcdede02c8a1
linnea <linnea.albertsson@gmail.com>
2023-01-03 11:05:42 +0100

    updated ratingDots to avoiud conflicts

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -1,65 +1,61 @@
-import { RatingBar } from '@aashu-dubey/react-native-rating-bar';
-import React, { ReactNode } from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { RatingBar } from "@aashu-dubey/react-native-rating-bar";
+import React, { ReactNode } from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 interface Props {
-	rating: number;
-	dotSize?: number,
-	single?: number,
-	width?: number
+  rating: number;
 }
-export const RatingDots = ({ rating, dotSize, single, width }: Props) => {
-	const handleRating = (value: number) => {
-		console.log(value);
-	};
+export const RatingDots = ({ rating }: Props) => {
+  const handleRating = (value: number) => {
+    console.log(value);
+  };
 
-	const styles = StyleSheet.create({
-		circle: {
-			width: dotSize ? dotSize : 20,
-			height: dotSize ? dotSize : 20,
-		},
-		container: {
-			width: width ? width : 120,
-		},
-		single: {
-			width: single ? single : 25,
-		}
-	});
+  const styles = StyleSheet.create({
+    circle: {
+      width: 20,
+      height: 20,
+    },
+    container: {
+      width: 120,
+    },
+    single: {
+      width: 25,
+    },
+  });
 
-	return (
-		<RatingBar
-			initialRating={rating}
-			direction="horizontal"
-			allowHalfRating
-			itemCount={5}
-			itemPadding={0}
-			ignoreGestures
-			rateStyles={{
-				container: styles.container,
-				starContainer: styles.single,
-			}}
-			ratingElement={{
-				full: (
-					<Image
-						style={styles.circle}
-						source={require('../assets/images/1.png')}
-					/>
-				),
-				half: (
-					<Image
-						style={styles.circle}
-						source={require('../assets/images/0_5.png')}
-					/>
-				),
-				empty: (
-					<Image
-						style={styles.circle}
-						source={require('../assets/images/0.png')}
-					/>
-				),
-			}}
-			onRatingUpdate={(value) => handleRating(value)}
-		/>
-	);
+  return (
+    <RatingBar
+      initialRating={rating}
+      direction="horizontal"
+      allowHalfRating
+      itemCount={5}
+      itemPadding={0}
+      ignoreGestures
+      rateStyles={{
+        container: styles.container,
+        starContainer: styles.single,
+      }}
+      ratingElement={{
+        full: (
+          <Image
+            style={styles.circle}
+            source={require("../assets/images/1.png")}
+          />
+        ),
+        half: (
+          <Image
+            style={styles.circle}
+            source={require("../assets/images/0_5.png")}
+          />
+        ),
+        empty: (
+          <Image
+            style={styles.circle}
+            source={require("../assets/images/0.png")}
+          />
+        ),
+      }}
+      onRatingUpdate={(value) => handleRating(value)}
+    />
+  );
 };
-
