7ec799e4d4d025bb562323c1f37df6bd2aad55eb
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2023-01-20 13:33:29 +0100

    ignores firebase error message

--- redux/store.ts
+++ redux/store.ts
@@ -2,6 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./signin";
 
 export default configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["user/setActiveUser"],
+      },
+    }),
   reducer: {
     user: userReducer,
   },
