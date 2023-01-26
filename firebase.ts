2b5a532600081f8c7ffc79775c78d35ba8ae7fb9
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2023-01-26 10:24:54 +0100

    adds bundleidentifier

--- firebase.ts
+++ firebase.ts
@@ -8,20 +8,21 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   //original
-  // apiKey: "AIzaSyCdpdQ0mUhv3D4fUi6QNPdf39pFLhTNnpA",
-  // authDomain: "prilla-c8eca.firebaseapp.com",
-  // projectId: "prilla-c8eca",
-  // storageBucket: "prilla-c8eca.appspot.com",
-  // messagingSenderId: "267350792334",
-  // appId: "1:267350792334:web:614ebc1c0a786003dc51df",
+  apiKey: "AIzaSyCdpdQ0mUhv3D4fUi6QNPdf39pFLhTNnpA",
+  authDomain: "prilla-c8eca.firebaseapp.com",
+  projectId: "prilla-c8eca",
+  storageBucket: "prilla-c8eca.appspot.com",
+  messagingSenderId: "267350792334",
+  appId: "1:267350792334:web:614ebc1c0a786003dc51df",
 
   // backup below
-  apiKey: "AIzaSyABzLETtOOPLJVOAwyqGJCDeYSyGAQPjLc",
-  authDomain: "prilla-backup.firebaseapp.com",
-  projectId: "prilla-backup",
-  storageBucket: "prilla-backup.appspot.com",
-  messagingSenderId: "151031722406",
-  appId: "1:151031722406:web:0b0011ad1e3b8a10dedf67",
+  //
+  //apiKey: "AIzaSyABzLETtOOPLJVOAwyqGJCDeYSyGAQPjLc",
+  // authDomain: "prilla-backup.firebaseapp.com",
+  // projectId: "prilla-backup",
+  // storageBucket: "prilla-backup.appspot.com",
+  // messagingSenderId: "151031722406",
+  // appId: "1:151031722406:web:0b0011ad1e3b8a10dedf67",
 };
 
 export const app = initializeApp(firebaseConfig);
